a2dd424d461d5674d6f29372bc6bb7493c0017fd
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-03-03 07:02:15 +0000

    agp: added AbstractTestConsumer.java in agp-test-common, started to us it in aggregating service for remisstatus and tidbokning

---
+++ agp-test-common/src/main/java/se/skltp/agp/test/consumer/AbstractTestConsumer.java
@@ -0,0 +1,42 @@
+package se.skltp.agp.test.consumer;
+
+import java.net.URL;
+
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+public abstract class AbstractTestConsumer<ServiceInterface> {
+
+	protected ServiceInterface _service = null;	
+
+    private Class<ServiceInterface> _serviceType;
+
+    /**
+     * Constructs a test consumer with a web service proxy setup for communication using HTTPS with Mutual Authentication
+     * 
+     * @param serviceType, required to be able to get the generic class at runtime, see http://stackoverflow.com/questions/3403909/get-generic-type-of-class-at-runtime
+     * @param serviceAddress
+     */
+	public AbstractTestConsumer(Class<ServiceInterface> serviceType, String serviceAddress) {
+
+		_serviceType = serviceType;
+		
+		JaxWsProxyFactoryBean proxyFactory = new JaxWsProxyFactoryBean();
+		proxyFactory.setServiceClass(getServiceType());
+		proxyFactory.setAddress(serviceAddress);
+
+		// Used for HTTPS
+		SpringBusFactory bf = new SpringBusFactory();
+		URL cxfConfig = this.getClass().getClassLoader().getResource("agp-cxf-test-consumer-config.xml");
+		if (cxfConfig != null) {
+			proxyFactory.setBus(bf.createBus(cxfConfig));
+		}
+
+		_service = proxyFactory.create(getServiceType()); 
+
+	}
+
+    Class<ServiceInterface> getServiceType() {
+    	return _serviceType;
+    }
+}
