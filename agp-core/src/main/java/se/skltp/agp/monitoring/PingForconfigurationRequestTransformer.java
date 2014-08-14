2602a5d418de86bf7199335b275f266a54b98656
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2014-08-14 15:08:56 +0000

    MIG-174:Implementera PingForConfiguration i Agp och aggregerande tjänster --------------------------------------------------------------------------
    För att uppgradera från agp 1.2.0 till agp 1.2.1[-version] och få stöd för anrop till PingForConfiguration
    
    #1: Uppdatera pom.xml
    
    <skltp.agp.version>1.2.1[-VERSION]</skltp.agp.version>
    
    #1: Nya properties för respektive GetAggregatedXYZ-config att uppdateras:
    
    # New properties inrtoduced by PingForConfiguration
    PINGFORCONFIGURATION_INBOUND_ENDPOINT=http://localhost:8080/agp/<getaggregatedxyz>/itintegration/monitoring/PingForConfiguration/1/rivtabp21
    ENGAGEMANGSINDEX_PINGFORCONFIGURATION_ENDPOINT=${PINGFORCONFIGURATION_TESTSTUB_INBOUND_ENDPOINT}
    ENGAGEMANGSINDEX_PINGFORCONFIGURATION_SERVICE_TIMEOUT_MS=3000
    APPLICATION_NAME=<GetAggregatedXYZ>
    
    PINGFORCONFIGURATION_TESTSTUB_INBOUND_ENDPOINT=http://localhost:8080/ei/teststub/itintegration/monitoring/PingForConfiguration/1/rivtabp21
    
    #2: Uppdatera log4j.xml i respektive GetAggregatedXYZ 
    
    	<!-- For PingForConfiguration logging only -->
         <appender name="ROLLINGFILE_MONITORING" class="org.apache.log4j.RollingFileAppender">
            <param name="file" value="${mule.home}/logs/mule-app-GetAggregatedXYZ-monitoring.log" />
            <param name="MaxFileSize" value="1000KB" />
            <param name="MaxBackupIndex" value="3" />
            <layout class="org.apache.log4j.PatternLayout">
                <param name="ConversionPattern" value="%d %-5p %-30c - %m%n" />
            </layout> 
        </appender>
    
            <!-- Monitoring logs to separate log file, additivity makes sure not logged twice -->
        <logger name="se.skltp.agp.monitoring" additivity="false">
            <level value="INFO" />
            <appender-ref ref="ROLLINGFILE_MONITORING"/>
        </logger>

---
+++ agp-core/src/main/java/se/skltp/agp/monitoring/PingForconfigurationRequestTransformer.java
@@ -0,0 +1,53 @@
+package se.skltp.agp.monitoring;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.riv.itintegration.monitoring.v1.PingForConfigurationType;
+
+public class PingForconfigurationRequestTransformer extends AbstractMessageTransformer {
+
+	private static final Logger log = LoggerFactory.getLogger(PingForconfigurationRequestTransformer.class);
+	
+	private String engagemangsIndexHsaId;
+	
+	/**
+	 * The logical address to use when addressing EI, needed if PingForConfiguration goes through VP
+	 * @param engagemangsIndexHsaId
+	 */
+	public void setEngagemangsIndexHsaId(String engagemangsIndexHsaId) {
+		this.engagemangsIndexHsaId = engagemangsIndexHsaId;
+	}
+	
+    /**
+     * Message aware transformer that ...
+     */
+    @Override
+    public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+
+        // Perform any message aware processing here, otherwise delegate as much as possible to pojoTransform() for easier unit testing
+        return pojoTransform(message.getPayload(), outputEncoding);
+    }
+
+	/**
+     * Simple pojo transformer method that can be tested with plain unit testing...
+	 */
+	protected Object pojoTransform(Object src, String encoding) throws TransformerException {
+
+		log.debug("Transforming payload: {}", src);
+		
+		//Forward the original reguest, but with logicalAddress=engagemangsIndexHsaId
+		Object[] reqList = (Object[])src;
+		PingForConfigurationType forwardRequestIn = (PingForConfigurationType)reqList[1];
+		Object[] reqOutList = new Object[] {engagemangsIndexHsaId, forwardRequestIn};
+
+		log.debug("Forward PingForConfiguration request to EI using logical address {}", engagemangsIndexHsaId);
+		
+		log.debug("Transformed payload: {}", reqOutList);
+		
+		return reqOutList;
+	}
+}
---
+++ agp-core/src/main/java/se/skltp/agp/monitoring/PingForconfigurationResponseTransformer.java
@@ -0,0 +1,69 @@
+package se.skltp.agp.monitoring;
+
+import java.util.Date;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soitoolkit.commons.mule.util.ThreadSafeSimpleDateFormat;
+
+import se.riv.itintegration.monitoring.v1.ConfigurationType;
+import se.riv.itintegration.monitoring.v1.PingForConfigurationResponseType;
+
+public class PingForconfigurationResponseTransformer extends AbstractMessageTransformer {
+
+	private static final Logger log = LoggerFactory.getLogger(PingForconfigurationResponseTransformer.class);
+	
+	private ThreadSafeSimpleDateFormat dateFormat = new ThreadSafeSimpleDateFormat("yyyyMMddhhmmss");
+	
+	private String appName;
+	
+	/**
+	 * Set the application name to be used in response of a PingForCOnfiguration
+	 * @param appName
+	 */
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	
+    /**
+     * Message aware transformer that ...
+     */
+    @Override
+    public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+
+        // Perform any message aware processing here, otherwise delegate as much as possible to pojoTransform() for easier unit testing
+        return pojoTransform(message.getPayload(), outputEncoding);
+    }
+
+	/**
+     * Simple pojo transformer method that can be tested with plain unit testing...
+	 */
+	protected Object pojoTransform(Object src, String encoding) throws TransformerException {
+
+		log.debug("Transforming payload: {}", src);
+		
+		log.debug("PingForConfiguration requested for {}", appName);
+		
+		PingForConfigurationResponseType response = new PingForConfigurationResponseType();
+		response.setPingDateTime(dateFormat.format(new Date()));
+		response.getConfiguration().add(createConfigurationInfo("Applikation", appName));
+		
+		log.debug("PingForConfiguration response returned for {}", appName);
+		
+		log.debug("Transformed payload: ", response);
+		
+		return response;
+	}
+	
+	private ConfigurationType createConfigurationInfo(String name, String value) {
+		log.debug("PingForConfiguration config added [{}: {}]", name, value);
+		
+		ConfigurationType configurationInfo = new ConfigurationType();
+		configurationInfo.setName(name);
+		configurationInfo.setValue(value);
+		return configurationInfo;
+	}
+}
