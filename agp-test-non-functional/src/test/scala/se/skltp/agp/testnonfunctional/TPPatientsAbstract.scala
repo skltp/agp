b8869aafad0807db03d3d206ed451f300b059071
Martin Flower <martin.flower@callistaenterprise.se>
2015-11-27 14:51:41 +0100

    SERVICE-404 - New simplified base classes for Gatling tests

---
+++ agp-test-non-functional/src/test/scala/se/skltp/agp/testnonfunctional/TPPatientsAbstract.scala
@@ -0,0 +1,69 @@
+package se.skltp.agp.testnonfunctional
+
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import se.skltp.agp.testnonfunctional.scenarios.GetAggregatedSomeScenario
+import io.gatling.core.structure.PopulatedScenarioBuilder
+
+/**
+ * Robustness test aggregated service.
+ * Runs against all patients (happy path, exceptions, delays)
+ */
+abstract class TPPatientsAbstract extends Simulation {
+
+//  val testDuration            =  12 hours
+//  val numberOfConcurrentUsers =   5
+//  val rampDuration            =   1 minute
+//  val minWaitDuration         =   2 seconds
+//  val maxWaitDuration         =   4 seconds
+  
+  val numberOfConcurrentUsers:Int = if (System.getProperty("numberOfConcurrentUsers") != null && !System.getProperty("numberOfConcurrentUsers").isEmpty()) {
+                                        new Integer(System.getProperty("numberOfConcurrentUsers"))
+                                    } else {
+                                        5
+                                    }
+
+  val testDuration:Duration       = if (System.getProperty("testDuration") != null && !System.getProperty("testDuration").isEmpty()) {
+                                       Duration((new Integer(System.getProperty("testDuration"))).longValue(), "minutes")
+                                    } else {
+                                       12 hours
+                                    }
+  
+  val rampDuration:FiniteDuration = if (System.getProperty("rampDuration") != null && !System.getProperty("rampDuration").isEmpty()) {
+                                        FiniteDuration((new Integer(System.getProperty("rampDuration"))).longValue(), "seconds")
+                                    } else {
+                                        1 minute
+                                    }
+  
+  val minWaitDuration:Duration    = if (System.getProperty("minWaitDuration") != null && !System.getProperty("minWaitDuration").isEmpty()) {
+                                        Duration((new Integer(System.getProperty("minWaitDuration"))).longValue(), "seconds")
+                                    } else {
+                                        2 seconds
+                                    }
+  
+  val maxWaitDuration:Duration    = if (System.getProperty("maxWaitDuration") != null && !System.getProperty("maxWaitDuration").isEmpty()) {
+                                        Duration((new Integer(System.getProperty("maxWaitDuration"))).longValue(), "seconds")
+                                    } else {
+                                        4 seconds
+                                    }
+  
+  val patientsFileName:String     = if (System.getProperty("patientsFileName") != null && !System.getProperty("patientsFileName").isEmpty()) {
+                                        System.getProperty("patientsFileName")
+                                    } else {
+                                        "patients.csv"
+                                    }
+  
+  def patients(serviceName:String, urn:String, responseElement:String, responseItem:String) 
+                 = scenario("patients")
+                 .during(testDuration) {
+                    feed(csv(patientsFileName).circular)
+                   .exec(GetAggregatedSomeScenario.request(serviceName, urn, responseElement, responseItem))
+                   .pause(minWaitDuration, maxWaitDuration)
+                  }
+  
+   def setUpAbstract(serviceName:String, urn:String, responseElement:String, responseItem:String, baseUrl:String) : PopulatedScenarioBuilder = {
+     patients(serviceName, urn, responseElement, responseItem).inject(rampUsers(numberOfConcurrentUsers) over (rampDuration)).protocols(http.baseURL(baseUrl).disableResponseChunksDiscarding) 
+   }
+}
