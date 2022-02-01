dfbbbf5f25226af44825a54ad27c6474a44d4b8f
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2022-02-01 10:07:29 +0100

    preStop file watch, first version

--- agp-application/src/main/java/se/skltp/aggregatingservices/processors/CreateResponseProcessor.java
+++ agp-application/src/main/java/se/skltp/aggregatingservices/processors/CreateResponseProcessor.java
@@ -23,6 +23,7 @@ import org.apache.cxf.headers.Header;
 import org.apache.cxf.headers.Header.Direction;
 import org.apache.cxf.message.MessageContentsList;
 import org.apache.cxf.staxutils.StaxUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import se.skltp.aggregatingservices.aggregate.AggregatedResponseResults;
 import se.skltp.aggregatingservices.api.AgpServiceFactory;
@@ -39,6 +40,18 @@ public class CreateResponseProcessor implements Processor {
 
   private static final ObjectFactory OF_HEADERS = new ObjectFactory();
   private static final JaxbUtil jaxbUtil = new JaxbUtil(ProcessingStatusType.class);
+
+  public boolean getCloseConnections() {
+    return closeConnections;
+  }
+
+  public void setCloseConnections(boolean closeConnections) {
+    this.closeConnections = closeConnections;
+  }
+
+  private boolean closeConnections = false;
+
+
 
   @Override
   public void process(Exchange exchange) throws Exception {
@@ -62,6 +75,9 @@ public class CreateResponseProcessor implements Processor {
     exchange.setProperty(LOG_ENGAGEMENT_PROCESSING_RESULT, exchange.getProperty(ENGAGEMENT_PROCESSING_RESULT));
 
     exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 200);
+    if (closeConnections) {
+      exchange.getIn().setHeader(HttpHeaders.CONNECTION, "close");
+    }
   }
 
   private void validateProcessingStatus(Exchange exchange, AggregatedResponseResults aggregatedResponseResults) {
---
+++ agp-application/src/main/java/se/skltp/aggregatingservices/route/PreStopRoute.java
@@ -0,0 +1,42 @@
+package se.skltp.aggregatingservices.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import se.skltp.aggregatingservices.processors.PreStopProcessor;
+
+import java.io.File;
+
+@Component
+public class PreStopRoute extends RouteBuilder {
+
+  public static final String PRE_STOP_ROUTE = "prestop-route";
+
+  @Autowired
+  PreStopProcessor preStopProcessor;
+
+  private String preStopFilePath = "/var/spool/agp-camel/prestopFile";
+
+  @Override
+  public void configure() {
+    if (preStopFilePath == null || preStopFilePath.length() == 0) return;
+
+    File preStopFile = new File(preStopFilePath);
+    File directory = preStopFile.getParentFile();
+    if (!directory.exists() || !directory.isDirectory()) {
+      log.error("PreStopFile directory {} does not exist.", directory);
+      return;
+    }
+
+    preStopProcessor.setPreStopFile(preStopFile);
+    String fileName = preStopFile.getName();
+    String directoryName = directory.toString();
+    log.info("Watching {} for preStopFile {}", directoryName, fileName);
+
+    String fileWatchUri = String.format("file-watch:%s?antInclude=%s", directoryName, fileName);
+
+    from(fileWatchUri)
+            .routeId(PRE_STOP_ROUTE)
+            .process(preStopProcessor);
+  }
+}
